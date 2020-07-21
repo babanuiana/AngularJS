03ec3260c54b9247fa52d50c9c2de7bba0efc51e
Babanu <ibabanu@techteam.com>
2020-07-22 02:08:31 +0300

    update

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
     'ngRoute',
-    'myApp.view1',
+    'myApp.index',
     'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
-    $routeProvider.otherwise({ redirectTo: '/view1' });
+    $routeProvider.otherwise({ redirectTo: '/movies' });
 }]);
--- app/moviesPage/index.js
+++ app/moviesPage/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // https://api.themoviedb.org/3/movie/now_playing?api_key=fc298428bb77d2a10fb5e0bc411eb836
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.index', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {
-        templateUrl: 'view1/view1.html',
-        controller: 'View1Ctrl'
+    $routeProvider.when('/movies', {
+        templateUrl: 'moviesPage/index.html',
+        controller: 'moviesCtrl'
     });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('moviesCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.sendRequest = function() {
         $http.get("https://api.themoviedb.org/3/movie/now_playing?api_key=fc298428bb77d2a10fb5e0bc411eb836")
