3e1e885a6d287087ce6c3cbe435c7303cdaa6abf
Babanu <ibabanu@techteam.com>
2020-07-22 14:47:08 +0300

    added movie details request

--- app/moviesPage/index.js
+++ app/moviesPage/index.js
@@ -1,6 +1,36 @@
 'use strict';
 
-// https://api.themoviedb.org/3/movie/now_playing?api_key=fc298428bb77d2a10fb5e0bc411eb836
+function MainController($scope, $http) {
+
+    const key = 'fc298428bb77d2a10fb5e0bc411eb836';
+    const API = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
+    const movieDetails = "https://api.themoviedb.org/3/movie/";
+    const test = API + key;
+
+    console.log('test', test);
+
+    $scope.sendRequest = function() {
+        $http.get(test)
+            .then(function(response) {
+                $scope.movies = response.data.results;
+            }, function(reason) {
+                // error
+            });
+    }
+
+    $scope.movieDetails = function(movie) {
+
+        const movieId = movie.id;
+        const lang = '?api_key=fc298428bb77d2a10fb5e0bc411eb836&language=en-US';
+        const url = movieDetails + movieId + lang;
+
+        $http.get(url)
+            .then(function(response) {
+                console.log(response);
+            });
+    }
+}
+
 angular.module('myApp.index', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -10,14 +40,8 @@ angular.module('myApp.index', ['ngRoute'])
     });
 }])
 
-.controller('moviesCtrl', ['$scope', '$http', function($scope, $http) {
-
-    $scope.sendRequest = function() {
-        $http.get("https://api.themoviedb.org/3/movie/now_playing?api_key=fc298428bb77d2a10fb5e0bc411eb836")
-            .then(function(response) {
-                $scope.movies = response.data.results;
-                console.log(response.data.results);
-            });
-    }
-
-}]);
+.controller('moviesCtrl', [
+    '$scope',
+    '$http',
+    MainController
+]);
